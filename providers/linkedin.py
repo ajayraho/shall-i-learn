7a04b8314a4761dc5fefa967df02802aff599c06
ajayraho <ajayraho@ymail.com>
2022-06-04 07:11:08 +0530

    BugWork: LinkedinProvider

--- providers/linkedin.py
+++ providers/linkedin.py
@@ -5,14 +5,16 @@ class LinkedinProvider:
   def __init__(self, query):
     self.query = query
   def getJobs(self):
+    jc=""
+    nc=""
     try:
       req=requests.get(url='https://www.linkedin.com/jobs/search?keywords={}&location=&geoId=&trk=homepage-jobseeker_jobs-search-bar_search-submit&position=1&pageNum=0'.format(self.query))
       soup=BeautifulSoup(req.text,"html.parser")
-      jc=soup.find_all("span",class_="results-context-header__job-count")
-      nc=soup.find_all("span",class_="results-context-header__new-jobs")
+      jc=soup.find("span",class_="results-context-header__job-count")
+      nc=soup.find("span",class_="results-context-header__new-jobs")
       return {
-        'liJobs':''.join(filter(str.isdigit,jc[0].text.strip())),
-        'liNewJobs':''.join(filter(str.isdigit,nc[0].text.strip()))
+        'liJobs':''.join(filter(str.isdigit,jc.text.strip())),
+        'liNewJobs':''.join(filter(str.isdigit,nc.text.strip()))
       }
     except Exception as e:
-      return {'liJobs':0,'liNewJobs':0, 'linkedinError': str(e)}
+      return {'liJobs':0,'liNewJobs':0, 'linkedinError': str(e), 'jc':jc.text, 'nc':nc.text}
